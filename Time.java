596114c96fff5b96dd21aecd0e90e6f9e64d903b
sulkowski99 <56975114+sulkowski99@users.noreply.github.com>
2019-10-27 15:58:08 +0100

    Update Time.java

--- Time.java
+++ Time.java
@@ -1,11 +1,13 @@
 package pack.com;
 
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.io.IOException;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Time extends TimerTask {
     private int tr;
+    int PORT = 9091;
 
     public Time(int tr) {
         this.tr = tr;
@@ -13,12 +15,32 @@ public class Time extends TimerTask {
 
     @Override
     public void run() {
+        //CZAS
+        Calendar hr = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        String time = sdf.format(hr.getTime());
 
-
+        //POBIERA KLIENTÓW DO WYSŁANIA CZASU (MOZLIWE ŻE OD RUSZENIA ZEGARA KTOS DOLACZYŁ)
+        Map<Integer, String> clientMap = Server.getClientMap();
         System.out.println("Time remaining: " + tr + " seconds");
-        tr = tr - 10;
-        if (tr < 0) {
-            System.out.println("XD");
+
+        try {
+            tr = tr - 10;
+            DatagramSocket serverSocket = new DatagramSocket(PORT);
+            if (tr < 0) {
+                String t = Integer.toString(tr);
+                for (Map.Entry<Integer, String> entry : clientMap.entrySet()) {
+                    int v = entry.getKey();
+                    String temp = "TM?" + time + "<<ID?" + entry.getValue() + "<<OP?Pozostaly_Czas" + t;
+                    InetAddress ia = InetAddress.getLocalHost();
+                    byte[] idans = (temp).getBytes();
+                    serverSocket.send(new DatagramPacket(idans, idans.length, ia, v));
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
