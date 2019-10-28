324b72d1ebf9b5fc026864f48f8d82ecbab3008c
sulkowski99 <56975114+sulkowski99@users.noreply.github.com>
2019-10-28 13:17:09 +0100

    Update Client.java

--- Client.java
+++ Client.java
@@ -7,59 +7,120 @@ import java.net.InetAddress;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class Client {
     private static final int SERVER_PORT=9090;
-    public int id;
-
+    static void confirm(DatagramSocket clientSocket) throws IOException {
+        Calendar hr = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        String time = sdf.format(hr.getTime());
+        String response = "OP?Wiadomosc_Dostarczona<<TM?" + time+"<<";
+        byte[] resp = (response).getBytes();
+        InetAddress ia = InetAddress.getLocalHost();
+        DatagramPacket clientPacket = new DatagramPacket(resp, resp.length, ia, SERVER_PORT);
+        clientSocket.send(clientPacket);
+    }
 
     public static void main(String[] args) throws IOException {
         try {
+            String id="";
             Calendar hr = Calendar.getInstance();
             SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-
             String time1 = sdf.format(hr.getTime());
 
             DatagramSocket clientSocket = new DatagramSocket();
-            String msg = "OP?sesja<<TM?"+time1+"<<";
+            String msg = "OP?Sesja<<TM?"+time1+"<<";
             byte [] b= msg.getBytes();
             InetAddress ia = InetAddress.getLocalHost();
             DatagramPacket clientPacket = new DatagramPacket(b, b.length, ia, SERVER_PORT);
-            clientSocket.send(clientPacket);
+            clientSocket.send(clientPacket);//Zapytanie o  sesję
+            System.out.println("Wyslano zapytanie o ID sesji");
+
+
+            b = new byte[1024];
+            DatagramPacket serverPocket = new DatagramPacket(b, b.length);
+            clientSocket.receive(serverPocket);//Odbiór potwierdzenia
+            String received = new String(serverPocket.getData());
+            //Client.confirm(clientSocket);//Potwierdzenie
+            String[] t = received.split("[A-Z]{2}\\?|<<[A-Z]{2}\\?|<<");
+            for(String x:t)
+            {
+                System.out.println(x);
+                //line=line +x;
+            }
+            clientSocket.receive(serverPocket);//Odbiera ID sesji
+            Client.confirm(clientSocket);//Wysyła potwierdzenie
+
+            received = new String(serverPocket.getData());
+          //  t = received.split("[A-Z]{2}\\?|<<[A-Z]{4}\\?|<<");
+            //Łopatologiczne wprowadzenie ID
+           if(received.charAt(13)!='<')
+           {
+               id=id+received.charAt(13);
+           }
+           if(received.charAt(14)!='<')
+           {
+                id=id+received.charAt(14);
+           }
+           if(received.charAt(15)!='<')
+           {
+                id=id+received.charAt(15);
+           }
+           System.out.println("\nPrzyznane ID sesji:"+id);
 
             while(true) {
-                Scanner input = new Scanner(System.in);
-                String task = input.nextLine();//Wpisuje polecenie
-                //Następuje sprawdzanie polecenia
-        /*    if((task.substring(0,2).equals("OD")&&task.substring(0,2).equals("ID")&&task.substring(0,2).equals("OP"))||task.length()<5)
-            {
-                System.out.println("[Client]:Wrong command: "+task);
-            }
-            else{
-                if(task.charAt(2)!='?'|| !task.substring(task.length()-2,task.length()).equals("<<"))
+                String line="";
+                clientSocket.receive(serverPocket);//Odbiera info o rozpoczęciu
+                Client.confirm(clientSocket);//Wysyła Potwierdzenie
+                received = new String(serverPocket.getData());
+                t = received.split("[A-Z]{2}\\?|<<[A-Z]{4}\\?|<<");
+                for(String x:t)
                 {
-                    System.out.println("[Client]:Wrong Command: "+task.substring(task.length()-2,task.length()) );
+                    System.out.println(x);
+                    line=line +x;
                 }
-                else
-                {*/
-                b = (task).getBytes();
-                ia = InetAddress.getLocalHost();
-                clientPacket = new DatagramPacket(b, b.length, ia, SERVER_PORT);
-                clientSocket.send(clientPacket);
+                if(line.contains("Pozostaly_Czas"))
+                {
+                    System.out.println(line);
+                }
+                if(line.contains("Podaj_Liczbe"))
+                {
+                    time1 = sdf.format(hr.getTime());
+                    System.out.println(line);
+                    Scanner input = new Scanner(System.in);
+                    String task = input.nextLine();
+                    System.out.println("LICZBA: "+task);
+                    task=new String("TM?"+time1+"<<ID?"+id+"<<OD?"+task+"<<");//Wpisuje liczbe
+                    b = (task).getBytes();
+                    ia = InetAddress.getLocalHost();
+                    clientPacket = new DatagramPacket(b, b.length, ia, SERVER_PORT);
+                    clientSocket.send(clientPacket);//Wysyła liczbę
+                    clientSocket.receive(serverPocket);//Otrzymuje potwierdzenie dostarczenia
+                    received = new String(serverPocket.getData());
+                    t = received.split("[A-Z]{2}\\?|<<[A-Z]{4}\\?|<<");
+                    for(String x:t)
+                    {
+                        System.out.println(x);
+                        //line=line +x;
+                    }
+                    clientSocket.receive(serverPocket);//Otrzymuje odpowiedź
+                    received = new String(serverPocket.getData());
+                    t = received.split("[A-Z]{2}\\?|<<[A-Z]{4}\\?|<<");
+                    for(String x:t)
+                    {
+                        System.out.println(x);
+                        //line=line +x;
+                    }
+                    Client.confirm(clientSocket);//Wysyła Potwierdzenie
 
-                // DatagramSocket serverSocket=new DatagramSocket();
-                b = new byte[1024];
-                DatagramPacket serverPocket = new DatagramPacket(b, b.length);
+                }
+                /*
+                serverPocket = new DatagramPacket(b, b.length);
                 clientSocket.receive(serverPocket);
                 String str = new String(serverPocket.getData());
-                str="OP?Sesja<<ID?1032<<TM?21:37:55<<";
-                String[] t =str.split("[A-Z]{2}\\?|<<[A-Z]{2}\\?|<<");
-                for(String x:t) System.out.print(x+" ");
-
+                System.out.println(str);
+                */
             }
-           // temp=task.substring(0,1); //Wyciągam z
 
         } catch(IOException e){
                 e.printStackTrace();
