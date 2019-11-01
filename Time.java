d8bb845303f0513a054c0a0a5965566491ad3275
Dodominionek <56478965+Dodominionek@users.noreply.github.com>
2019-11-01 18:01:45 +0100

    Update Time.java

--- Time.java
+++ Time.java
@@ -6,57 +6,55 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Time extends TimerTask {
-    private int tr;
-    int port = 9091;
+        private int tr;
+        int port = 9091;
 
-    public Time(int tr) {
-        this.tr = tr;
-    }
-
-    @Override
-    public void run() {
-        //CZAS
-        Calendar hr = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-        String time = sdf.format(hr.getTime());
-
-        //POBIERA KLIENTÓW DO WYSŁANIA CZASU (MOZLIWE ŻE OD RUSZENIA ZEGARA KTOS DOLACZYŁ)
-        Map<Integer, String> clientMap = Server.getClientMap();
-
-
-        try {
-            if (tr > 0) {
-                System.out.println("Pozostaly czas: " + tr + "s");
-                String t = Integer.toString(tr);
-                for (Map.Entry<Integer, String> entry : clientMap.entrySet()) {
-                    int v = entry.getKey();
-                    String temp = "TM?" + time + "<<ID?" + entry.getValue() + "<<OP? Pozostaly_Czas:" + t;
-                    InetAddress ia = InetAddress.getLocalHost();
-                    byte[] idans = (temp).getBytes();
-                    //    serverSocket.send(new DatagramPacket(idans, idans.length, ia, v));
-                    DatagramPacket clientPacket = new DatagramPacket(idans, idans.length, ia, v);
-                    Server.serverSocket.send(clientPacket);
-                }
-            }
-            if (tr < 0) {
-                for (Map.Entry<Integer, String> entry : clientMap.entrySet()) {
-                    int v = entry.getKey();
-                    String temp = "TM?" + time + "<<ID?" + entry.getValue() + "<<OP? Upłynął_Czas_Rozgrywki";
-                    InetAddress ia = InetAddress.getLocalHost();
-                    byte[] idans = (temp).getBytes();
-                    //    serverSocket.send(new DatagramPacket(idans, idans.length, ia, v));
-                    DatagramPacket clientPacket = new DatagramPacket(idans, idans.length, ia, v);
-                    Server.serverSocket.send(clientPacket);
-                    this.cancel();
-                }
-            }
-
-        } catch (SocketException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        public Time(int tr) {
+                this.tr = tr;
         }
-        tr = tr - 10;
 
-    }
+        @Override
+        public void run() {
+                //CZAS
+                Calendar hr = Calendar.getInstance();
+                SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+                String time = sdf.format(hr.getTime());
+
+                //POBIERA KLIENTÓW DO WYSŁANIA CZASU (MOZLIWE ŻE OD RUSZENIA ZEGARA KTOS DOLACZYŁ)
+                Map<Integer, String> clientMap = Server.getClientMap();
+
+
+                try {
+                        if (tr > 0) {
+                                System.out.println("Pozostaly czas: " + tr + " sekund");
+                                String t = Integer.toString(tr);
+                                for (Map.Entry<Integer, String> entry : clientMap.entrySet()) {
+                                        int v = entry.getKey();
+                                        String temp = "TM?" + time + "<<ID?" + entry.getValue() + "<<OP? Pozostaly_Czas:" + t;
+                                        InetAddress ia = InetAddress.getLocalHost();
+                                        byte[] idans = (temp).getBytes();
+                                        //    serverSocket.send(new DatagramPacket(idans, idans.length, ia, v));
+                                        DatagramPacket clientPacket = new DatagramPacket(idans, idans.length, ia, v);
+                                        Server.serverSocket.send(clientPacket);
+                                }
+                        }
+                        if (tr < 0) {
+                                for (Map.Entry<Integer, String> entry : clientMap.entrySet()) {
+                                        int v = entry.getKey();
+                                        String temp = "TM?" + time + "<<ID?" + entry.getValue() + "<<OP? Upłynął_Czas_Rozgrywki";
+                                        InetAddress ia = InetAddress.getLocalHost();
+                                        byte[] idans = (temp).getBytes();
+                                        //    serverSocket.send(new DatagramPacket(idans, idans.length, ia, v));
+                                        DatagramPacket clientPacket = new DatagramPacket(idans, idans.length, ia, v);
+                                        Server.serverSocket.send(clientPacket);
+                                        this.cancel();
+                                }
+                        }
+                } catch (SocketException e) {
+                        e.printStackTrace();
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+                tr = tr - 10;
+        }
 }
